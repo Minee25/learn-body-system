f6e9214573abf14072e119d46420b7f34eea7837
Minee <oakmuansuk123@gmail.com>
2023-03-23 21:19:28 +0700

    fix all

--- lesson/lesson.js
+++ lesson/lesson.js
@@ -58,38 +58,3 @@ tabs.forEach((tab, index) => {
         tabs[index].classList.add("activev");
     });
 });
-
-
-
-
-
-
-
-const sidebar = document.querySelector(".sidebar");
-
-const toggleopen = document.querySelector(".toggle-open");
-const toggleclose = document.querySelector(".toggle-close");
-
-const logo = document.querySelector(".logo");
-const title = document.querySelector(".sidebar-title");
-
-
-// ////////////////////
-
-toggleopen.addEventListener("click", () => {
-    sidebar.classList.add("open");
-    toggleclose.classList.remove("hidden");
-    toggleopen.classList.add("hidden");
-
-    logo.classList.remove("hidden");
-    title.classList.remove("hidden");
-});
-
-toggleclose.addEventListener("click", () => {
-    sidebar.classList.remove("open");
-    toggleopen.classList.remove("hidden");
-    toggleclose.classList.add("hidden");
-
-    logo.classList.add("hidden");
-    title.classList.add("hidden");
-});
